80c6efd10663a357d7ebd4579e330d08ee6479e6
Aaron Gallagher <habnabit@gmail.com>
2013-04-27 15:16:46 -0700

    Adding a little debugging/UI.

--- identity/js/authentication.js
+++ identity/js/authentication.js
@@ -1,7 +1,11 @@
 navigator.id.beginAuthentication(function (persona_email) {
-  if (cert_email == persona_email) {
-    navigator.id.completeAuthentication();
-  } else {
-    navigator.id.raiseAuthenticationFailure("cert e-mail didn't match");
-  }
+  document.write('hello, ' + cert_email + '. are you ' + persona_email + '? ');
+  document.write(cert_email == persona_email? '(yes.)' : '(no!)');
+  window.setTimeout(function () {
+    if (cert_email == persona_email) {
+      navigator.id.completeAuthentication();
+    } else {
+      navigator.id.raiseAuthenticationFailure("cert e-mail didn't match");
+    }
+  }, 2000);
 });
